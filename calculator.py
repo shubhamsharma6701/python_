dbe6d7abdda13d1842ca806c172052fd0df56d1a
Shubham Sharma <eshubham6701@gmail.com>
2025-07-28 19:19:16 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+x = float(input("What is x? \n"))
+y = float(input("What is y?: \n"))
+
+# z = round(x + y)
+# z = round(x / y, 2)
+z = x / y
+
+print(f"{z:.2f}") # Rounding to 2 digits after the decimal point
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,24 @@
+
+# def main():
+#     name = input("What is your name? \n")
+#     hello(name)
+#     print(return_hello(name))
+
+# def hello(name="World"):
+#     print("Hello,", name)
+
+# def return_hello(name="World"):
+#     return "Hello, " + name
+
+# main()
+
+
+
+def main():
+    x = int(input("What is x? \n"))
+    print("x squared is", squared_x(x))
+
+def squared_x(x):
+    return x * x
+
+main()
